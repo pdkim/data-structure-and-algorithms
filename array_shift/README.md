dd836008ab34e349538e9672074b9a6c81a15a68
Phillip Kim <phildkim@gmail.com>
2018-05-20 22:34:08 -0700

    whiteboard 2 working

--- array_shift/README.md
+++ array_shift/README.md
@@ -1,8 +1,8 @@
 # Insert and shift middle index of array
-Create a function called insertShiftarray that takes an array and adds a new value within the array.
+Create a function called insertShiftarray that takes an array and adds a new value in the middle of the array.
 
 ## Challenge
-The value must be added to the array maintaining the order of all values from lowest to highest.
+The value must be added in the middle of the array without using any built in methods in javascript.
 
 ## Solution
-![whiteboard]()
+![whiteboard](assets/array_shift.JPG)
--- array_shift/array_shift.spec.js
+++ array_shift/array_shift.spec.js
@@ -1,21 +1,21 @@
 const insertShiftArray = require('./array_shift.js');
 
 describe('add value to array and maintain lowest to highest order', () => {
-  it('by placing the value in the middle of the array', () => {
-    let actual = insertShiftArray(3, [1, 2, 4, 5]);
-    let expected = [1, 2, 3, 4, 5];
-    expect(actual).toBe(expected);
+  it('by placing the value at index of 2 in the array', () => {
+    let actual = insertShiftArray([2, 4, 6, 8], 5);
+    let expected = [2, 4, 5, 6, 8];
+    expect(actual).toEqual(expected);
   });
 
-  it('by placing the value at the second [1] position in the array', () => {
-    let actual = insertShiftArray(6, [3, 9, 12]);
-    let expected = [3, 6, 9, 12];
-    expect(actual).toBe(expected);
+  it('by placing the value at index of 3 in the array', () => {
+    let actual = insertShiftArray([4, 8, 15, 23, 42], 16);
+    let expected = [4, 8, 15, 16, 23, 42];
+    expect(actual).toEqual(expected);
   });
 
   it('by placing the value at the end of the array', () => {
-    let actual = insertShiftArray(25, [5, 10, 15, 20]);
-    let expected = [5, 10, 15, 20, 25];
-    expect(actual).toBe(expected);
+    let actual = insertShiftArray([5, 10, 15, 20], 'test');
+    let expected = [5, 10, 'test', 15, 20];
+    expect(actual).toEqual(expected);
   });
 });
