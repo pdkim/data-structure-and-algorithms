b6e5bab2abc49ff1e9b99a820379fcfe7bfcc444
Phillip Kim <phildkim@gmail.com>
2018-05-28 16:30:56 -0700

    1 hour on coding met...

--- array_adjacent_product/__test__/array_adjacent_product.spec.js
+++ array_adjacent_product/__test__/array_adjacent_product.spec.js
@@ -2,17 +2,57 @@
 
 const LargestProduct = require('../lib/array_adjacent_product.js');
 
-describe('this is hard', () => {
+describe('Lergest Product', () => {
 
-  xit('testing', () => {
+  xit('should return highest product of adjacent numbers in the same row', () => {
     //first test
+    let arr = [1, 2, 3];
+    let actual = LargestProduct(arr);
+    expect(actual).toBe(6);
   });
 
-  xit('testing', () => {
+  xit('should return highest product of adjacent numbers in the same column', () => {
     //second test
+    let arr = [
+      [1],
+      [2],
+      [3],
+    ];
+    let actual = LargestProduct(arr);
+    expect(actual).toBe(6);    
   });
 
-  xit('testing', () => {
+  xit('should return highest product of adjacent numbers of diagonal numbers (South East)', () => {
     //third test
+    let arr = [
+      [1, 2],
+      [5, 4],
+      [3, 6],
+    ];
+    let actual = LargestProduct(arr);
+    expect(actual).toBe(30);   
+  });
+
+  xit('should return highest product of adjacent numbers of diagonal numbers (South West)', () => {
+    //third test
+    let arr = [
+      [1, 2],
+      [4, 5],
+      [6, 3],
+    ];
+    let actual = LargestProduct(arr);
+    expect(actual).toBe(30);   
+  });
+
+  xit('should return highest product of adjacent numbers of diagonal numbers (South East)', () => {
+    //third test
+    let arr = [ 
+      [ 1, 2, 1 ], 
+      [ 2, 4, 2 ], 
+      [ 3, 6, 8 ], 
+      [ 7, 8, 1 ], 
+    ];
+    let actual = LargestProduct(arr);
+    expect(actual).toBe(64);   
   });
 });
--- array_adjacent_product/lib/array_adjacent_product.js
+++ array_adjacent_product/lib/array_adjacent_product.js
@@ -1,8 +1,59 @@
 'use strict';
 
-function LargestProduct() {
-  //enter code here
+function LargestProduct(matrix) {
+  let maxProduct = 0;
+  const maxCol = matrix[0].length - 1;
+  const maxRow = matrix.length - 1;
 
+  for (let currentRow = 0; currentRow <= maxRow; currentRow++) {
+    for (let currentCol = 0; currentRow <= maxCol; currentCol++) {
+
+      let currentProduct = 0;
+
+      let check = 0;
+
+      let val = matrix[currentRow][currentCol];
+
+      maxProduct = Math.max(maxProduct, currentProduct);
+
+      //"East"
+      if (typeof (matrix[currentRow][currentCol +1]) === 'number') {
+        check = matrix[currentRow][currentCol + 1];
+        currentProduct = val * check;
+        if (maxProduct < currentProduct) {
+          maxProduct = currentProduct;
+        }
+      }
+
+      // "South"
+      // if(typeof(matrix[currentRow + 1][currentCol]) === 'number') {
+      //   check = matrix[currentRow + 1][currentCol];
+      //   currentProduct = val * check;
+      //   if(maxProduct < currentProduct) {
+      //     maxProduct = currentProduct;
+      //   }
+      // }
+
+      // "Southeast"
+      // if(typeof(matrix[currentRow + 1][currentCol + 1]) === 'number') {
+      //   check = matrix[currentRow + 1][currentCol + 1];
+      //   currentProduct = val * check;
+      //   if(maxProduct < currentProduct) {
+      //     maxProduct = currentProduct;
+      //   }
+      // }
+
+      // "Southwest"
+      // if(typeof(matrix[currentRow - 1][currentCol - 1]) === 'number') {
+      //   check = matrix[currentRow + 1][currentCol - 1];
+      //   currentProduct = val * check;
+      //   if(maxProduct < currentProduct) {
+      //     maxProduct = currentProduct;
+      //   }
+      // }
+    }
+  }
+  return maxProduct;
 }
 
 
