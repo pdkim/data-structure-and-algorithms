0b5b9acc4be7c6dc6a78edc270cdde5851aceea2
Phillip Kim <phildkim@gmail.com>
2018-06-11 22:41:00 -0700

    getting started

---
+++ ll_detect_loop/__test__/name.spec.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const name = require('../lib/name.js');
+
+describe('this is hard', () => {
+
+  xit('testing', () => {
+    //first test
+  });
+
+  xit('testing', () => {
+    //second test
+  });
+
+  xit('testing', () => {
+    //third test
+  });
+});
---
+++ ll_detect_loop/lib/name.js
@@ -0,0 +1,9 @@
+'use strict';
+
+function name() {
+  //enter code here
+
+}
+
+
+module.exports = name;
