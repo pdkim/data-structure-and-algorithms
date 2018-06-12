c5ae5f8a3dd5bb34181dbabcfa01ad0202575c99
Phillip Kim <phildkim@gmail.com>
2018-06-11 22:13:46 -0700

    getting started

---
+++ ll_merge/__test__/ll_merge.spec.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const Merge = require('../lib/ll_merge.js');
+
+describe('Merge module', () => {
+
+  it('should combine 2 linked lists into one', () => {
+    let listA = new Merge;
+    listA.append(1);
+
+    let listB = new Merge;
+    listB.append(2);
+
+    listA.combine(listA, listB);
+    let actual1 = listA.head.value;
+    let actual2 = listA.head.next.value;
+
+    let expected1 = 1;
+    let expected2 = 2;
+
+    expect(actual1).toBe(expected1);
+    expect(actual2).toBe(expected2);
+  });
+
+  xit('should alternate values between the 2 when merged', () => {
+    let actual = null;
+    expect(actual).toBeNull();
+  });
+
+  xit('should continue list when no values exist for the other list', () => {
+    let actual = null;
+    expect(actual).toBeNull();
+  });
+});
