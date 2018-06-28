527b892eae5711b4936addf5d4f60702084a3a1d
Phillip Kim <phildkim@gmail.com>
2018-06-27 21:33:11 -0700

    pseudo and readme created

---
+++ fifo_animal_shelter/__test__/fifo_animal_shelter.spec.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const name = require('../lib/fifo_animal_shelter.js');
+console.log(name);
+
+describe('this is hard', () => {
+
+  xit('testing', () => {
+    //first test
+  });
+
+  xit('testing', () => {
+    //second test
+  });
+
+  xit('testing', () => {
+    //third test
+  });
+});
