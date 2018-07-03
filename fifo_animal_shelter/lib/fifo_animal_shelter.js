c30c57549eff64ab07b05c068df0fb13078558f8
Phillip Kim <phildkim@gmail.com>
2018-07-02 21:33:20 -0700

    time limit reached.

--- fifo_animal_shelter/lib/fifo_animal_shelter.js
+++ fifo_animal_shelter/lib/fifo_animal_shelter.js
@@ -1,10 +1,65 @@
 'use strict';
 
-class AnimalShelter {
-  //enter code here
-  
-
+class Node {
+  constructor(name, type) {
+    this.name = name;
+    this.type = type;
+    this.next = null;
+  }
 }
+
+class AnimalShelter {
+
+  constructor() {
+    this.head = null;
+    this.foot = null;
+    this.next = null;
+  }
+
+  enqueue(name, type) {
+
+    let node = new Node(name, type);
+
+    if (!this.head) {
+      this.head = node;
+      this.foot = node;
+    }
+
+    let prevHead = this.head;
+    this.head = node;
+    node.next = prevHead;
+  }
+
+
+  dequeue(pref) {
+
+    let currNode = this.head;
+    let preNode = this.head;
+    let futureNode = currNode.next;
+
+    while (currNode.next) {
+      if (currNode.type === pref) {
+        currNode.next = null;
+        preNode.next = futureNode;
+        return preNode.name;
+      }
+      preNode.next = futureNode;
+      return currNode.name;
+    }
+    if (currNode.next === null) {
+      return currNode.name;
+    }
+    else {
+      preNode = currNode;
+      // console.log('prenode is ', preNode);
+      currNode = currNode.next;
+      // console.log('curr node is', currNode);
+      futureNode = currNode.next;
+      // console.log('future is ', futureNode);
+    }
+  }
+}
+
 
 
 module.exports = AnimalShelter;
